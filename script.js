a31e8b55754fe9821dee7b791cce3dc406ce5bd1
Ditt GitHub-brukernavn <adrian.koder@gmail.com>
2025-04-01 12:57:37 +0100

    fikset oppi mye i js

--- script.js
+++ script.js
@@ -50,123 +50,56 @@ bildeSomByttes.style.height = "30rem"
   bildeSomByttes.alt = "stand 19 okt 24 Torgallmenning";
   console.log(liID);
 });
-  VaneligeSpørsmål.addEventListener("click", () => {
-    console.log("VaneligeSpørsmål clicked");
-    meneskerILombo.innerHTML = meneskerILombo.innerHTML = `
+
+VaneligeSpørsmål.addEventListener("click", () => {
+  console.log("VaneligeSpørsmål clicked");
+  meneskerILombo.innerHTML = `
   <button id='vaneligSpørsmålHvorfor'>Hvorfor reiser de ikke bare tilbake?</button>
-  <button id='hvaVilDeOppnå'>Hva vil de oppnå i Norge?</button>
-`;
-    // const vaneligSpørsmålHvorfor = document.getElementById(
-    //   "vaneligSpørsmålHvorfor"
-    // );
-    vaneligSpørsmålHvorfor.addEventListener("click", () => {
-      meneskerILombo.innerHTML =
-        "<p><li>Det er grove brudd på menneskerettigheter der jeg kommer fra.</li></p>" +
-        "<p><li>Som kvinne mangler jeg rettigheter i hjemlandet mitt.</li></p>" +
-        "<p><li>Mitt land er styrt av diktatur, og jeg er i fare for å bli satt i fengsel og torturert.</li></p>";
-      meneskerILombo.style.padding = "25px";
-    });
-    hvaVilDeOppnå.addEventListener("click", () => {
-      meneskerILombo.innerHTML = `
-    <li>De vil arbeide for maten og betale skatt til fellesskapet</li>
-    <li>De vil ha rett til utanning</li>
-    <li>De vil ha rett til helsehjelp</li>
-    <li>De vil ha stemmerett</li>
-    <li>Rett til å leve som du og jeg</li>
-    <li> De vil ha rett til menneskeverd</li>`;
-      meneskerILombo.style.padding = "25px";
+  <button id='hvaVilDeOppnå'>Hva vil de oppnå i Norge?</button>`;
+  
+  // Legg til event listeners etter at innerHTML er satt
+  document.getElementById("vaneligSpørsmålHvorfor").addEventListener("click", () => {
+    meneskerILombo.innerHTML = `
+      <button id="tilbake">tilbake</button>
+      <p><li>Det er grove brudd på menneskerettigheter der jeg kommer fra.</li></p>
+      <p><li>Som kvinne mangler jeg rettigheter i hjemlandet mitt.</li></p>
+      <p><li>Mitt land er styrt av diktatur, og jeg er i fare for å bli satt i fengsel og torturert.</li></p>`;
+    meneskerILombo.style.padding = "25px";
+
+    // Legg til event listener for "tilbake"-knappen
+    document.getElementById("tilbake").addEventListener("click", () => {
+      VaneligeSpørsmål.click(); // Gjenopprett VaneligeSpørsmål
     });
   });
-  HvaBetyrLimbo.addEventListener("click", () => {
-    meneskerILombo.innerHTML = `Limbo beskrives som en mellomtilstand eller en tilstand av lammelse,
+
+  document.getElementById("hvaVilDeOppnå").addEventListener("click", () => {
+    meneskerILombo.innerHTML = `
+      <button id="tilbake">tilbake</button>
+      <li>De vil arbeide for maten og betale skatt til fellesskapet</li>
+      <li>De vil ha rett til utanning</li>
+      <li>De vil ha rett til helsehjelp</li>
+      <li>De vil ha stemmerett</li>
+      <li>Rett til å leve som du og jeg</li>
+      <li>De vil ha rett til menneskeverd</li>`;
+    meneskerILombo.style.padding = "25px";
+
+    // Legg til event listener for "tilbake"-knappen
+    document.getElementById("tilbake").addEventListener("click", () => {
+      VaneligeSpørsmål.click(); // Gjenopprett VaneligeSpørsmål
+    });
+  });
+});
+
+HvaBetyrLimbo.addEventListener("click", () => {
+  meneskerILombo.innerHTML = `Limbo beskrives som en mellomtilstand eller en tilstand av lammelse,
   hvor papirløse asylsøkere verken kan reise tilbake til hjemlandet eller starte et nytt liv i Norge. 
   Det oppleves som håpløst og uten mulighet for endring.
-  <a href= https://yemane.no/1> hvordan oppleves limbo </a>`;
-  });
+  <a href="https://yemane.no/1"> hvordan oppleves limbo </a>`;
+});
 
-  omArbeidet.addEventListener("click", () => {
-    meneskerILombo.innerHTML = `<p> Hvem er vi?
-  Gruppen består av lengeværende papirløse asylsøkere, inkludert kvinner, menn og barn, som har flyktet til Norge. De kjemper for:</p>
-
-<p><li>Arbeid</li><br>
-<li>Skolegang</li><br>
-<li>Helsehjelp</li><br>
-<li>Økonomiske velferdstjenester</p>
-</li>
-
-Papirløse eller irregulære migranter er personer som oppholder seg i Norge uten lovlig opphold. Mange er asylsøkere med endelig avslag. Flere i denne gruppen bor på asylmottak, særlig barnefamilier.
-
-De irregulære migrantene som, i frykt for tvangsretur eller av andre årsaker, velger å ikke bo på asylmottak – eller som ikke har rett til mottaksplass – lever på kanten av det norske samfunnet. De livnærer seg på uformell sosial støtte eller illegalt arbeid og er ekskludert fra det offentlige støtteapparatet.<br><br>
-
-Risikoen for utnyttelse er stor, både når det gjelder menneskehandel, andre overgrep og utnyttelse i det svarte arbeidsmarkedet (underbetaling, manglende arbeidsrettigheter og fravær av arbeidskontrakt).
-Både psykisk og somatisk helse kan være dårlig.
-De kan ha helsetilstander som kunne vært behandlet,
-men som utvikler seg til kroniske eller livstruende tilstander fordi de forblir ubehandlet.
-I mange tilfeller dreier det seg om omfattende psykososiale og helsemessige utfordringer.`;
-
-    liID.style.display = "none"; // Skjul liID
-    bildeSomByttes.src = "./stand 19 okt 24 Torgallmenning.jpg";
-    bildeSomByttes.alt = "stand 19 okt 24 Torgallmenning";
-    console.log(liID);
-
-    VaneligeSpørsmål.addEventListener("click", () => {
-      console.log("VaneligeSpørsmål clicked");
-      meneskerILombo.innerHTML = `
-      <button id='vaneligSpørsmålHvorfor'>Hvorfor reiser de ikke bare tilbake?</button>
-      <button id='hvaVilDeOppnå'>Hva vil de oppnå i Norge?</button>`;
-      bildeSomByttes.style.width = "30rem";
-      bildeSomByttes.style.height = "auto";
-      bildeSomByttes.src = "./image copy.png";
-      bildeSomByttes.alt = "presidenten";
-
-      // Legg til event listeners etter at innerHTML er satt
-      document
-        .getElementById("vaneligSpørsmålHvorfor")
-        .addEventListener("click", () => {
-          meneskerILombo.innerHTML = `
-        <p><li>Det er grove brudd på menneskerettigheter der jeg kommer fra.</li></p>
-        <p><li>Som kvinne mangler jeg rettigheter i hjemlandet mitt.</li></p>
-        <p><li>Mitt land er styrt av diktatur, og jeg er i fare for å bli satt i fengsel og torturert.</li></p>
-      `;
-          meneskerILombo.style.padding = "25px";
-        });
-
-      document.getElementById("hvaVilDeOppnå").addEventListener("click", () => {
-        meneskerILombo.innerHTML = `
-        <li>De vil arbeide for maten og betale skatt til fellesskapet</li>
-        <li>De vil ha rett til utanning</li>
-        <li>De vil ha rett til helsehjelp</li>
-        <li>De vil ha stemmerett</li>
-        <li>Rett til å leve som du og jeg</li>
-        <li>De vil ha rett til menneskeverd</li>
-      `;
-      bildeSomByttes.src = "./image copy.png";
-      bildeSomByttes.alt = "presidenten";
-        meneskerILombo.style.padding = "25px";
-      });
-    });
-  });
-
-  HvaBetyrLimbo.addEventListener("click", () => {
-    meneskerILombo.innerHTML = `Limbo beskrives som en mellomtilstand eller en tilstand av lammelse,
-    hvor papirløse asylsøkere verken kan reise tilbake til hjemlandet eller starte et nytt liv i Norge. 
-    Det oppleves som håpløst og uten mulighet for endring.
-    <a href="https://yemane.no/1"> hvordan oppleves limbo </a>`;
-  });
-
-  KontaktOss.addEventListener("click", () => {
-    console.log("KontaktOss clicked");
-  
-    meneskerILombo.innerHTML = `
+KontaktOss.addEventListener("click", () => {
+  console.log("KontaktOss clicked");
+  meneskerILombo.innerHTML = `
     <p>Vill du bli medlem eller har spørsmål Kontakt oss på telefon: <a href='tel:+4790038233'>telf.90038233</a></p>
-    <p>Eller e-post: <a href='mailto:ketil_hindenes@hotmail.com'>ketil_hindenes@hotmail.com</a></p>
-  `
-    
-//  <a href="https://vipps.no/pay?link=DIN_GENERERTE_LENKE" target="_blank">Betal med Vipps</a></a>  ;
-
-
-
-
-    // Skjul liID
-  });
-
+    <p>Eller e-post: <a href='mailto:ketil_hindenes@hotmail.com'>ketil_hindenes@hotmail.com</a></p>`;
+});
